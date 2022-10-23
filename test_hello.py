e8d41d85d98101e99591e4aaf678bf2ccfe96199
Alice Strawther <alice@strawther.us>
2022-10-23 15:47:45 -0500

    Update test_hello.py

--- test_hello.py
+++ test_hello.py
@@ -1,5 +1,20 @@
-from hello import add
+from hello import toyou, add, subtract
 
 
-def test_add():
-    assert 2 == add(1, 1)
+def setup_function(function):
+    print("Running Setup: %s" % {function.__name__})
+    function.x = 10
+
+def teardown_function(function):
+    print("Running Teardown: %s" % {function.__name__})
+    del function.x
+
+
+
+
+### Run to see failed test
+#def test_hello_add():
+# assert subtract(test_hello_subtract.x) == 12
+
+def test_hello_subtract():
+    assert subtract(test_hello_subtract.x) == 9 
